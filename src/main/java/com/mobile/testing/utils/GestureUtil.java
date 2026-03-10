207e3e8679930fc0327a40e0d19b0cc398acf562
ghadasawallha04-collab <ghada.sawallha04@gmail.com>
2026-03-10 20:33:02 +0200

    Update GestureUtil.java

--- src/main/java/com/mobile/testing/utils/GestureUtil.java
+++ src/main/java/com/mobile/testing/utils/GestureUtil.java
@@ -1,11 +1,9 @@
 package com.mobile.testing.utils;
-
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.interactions.PointerInput;
 import org.openqa.selenium.interactions.Sequence;
-
 import java.time.Duration;
 import java.util.Collections;
 
